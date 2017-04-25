bd39a0335aecb6b37e90f8763459e25afc1869b1
zhangjunlei26 <zhangjunlei26@gmail.com>
2017-04-26 02:39:19 +0800

    init

---
+++ lisp/init-misc.el
@@ -0,0 +1,17 @@
+;;----------------------------------------------------------------------------
+;; Misc config - yet to be placed in separate files
+;;----------------------------------------------------------------------------
+(add-auto-mode 'tcl-mode "Portfile\\'")
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(add-hook 'prog-mode-hook 'goto-address-prog-mode)
+(setq goto-address-mail-face 'link)
+
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+(setq-default regex-tool-backend 'perl)
+
+(add-auto-mode 'conf-mode "Procfile")
+
+
+(provide 'init-misc)
